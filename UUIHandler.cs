919106f5e9eb35b9b7ab4414778308561fee87b5
sukggom <sukggom@naver.com>
2025-09-05 18:15:28 +0900

    Add files via upload

---
+++ UUIHandler.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UG.Framework
+{
+    public class UUIHandler
+    {
+        private static int Count = 0;
+
+        private int Handle;
+
+        internal UUIHandler()
+        {
+            Handle = ++Count;
+        }
+
+        public int Get()
+        {
+            return Handle;
+        }
+        public override string ToString()
+        {
+            return Handle.ToString();
+        }
+
+        public static bool operator ==(UUIHandler a, UUIHandler b)
+        {
+          
+            return a.Handle.Equals(b.Handle);
+        }
+
+        public static bool operator !=(UUIHandler a, UUIHandler b)
+        {
+            return !a.Handle.Equals(b.Handle);
+        }
+
+        public override int GetHashCode()
+        {
+            return Handle.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            return this.Equals((UUIHandler)obj);
+        }
+
+        public bool Equals(UUIHandler b)
+        {
+            if (this.Handle.Equals(b.Handle) == true)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static readonly UUIHandler Invalid = new UUIHandler() { Handle = -1 };
+    }
+}
